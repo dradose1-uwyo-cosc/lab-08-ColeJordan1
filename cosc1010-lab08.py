66fe8fdead57fe7c749f1b94c94aa1c1580a91a8
ColeJordan1 <cjorda22@uwyo.edu>
2024-11-07 16:05:59 -0700

    Update cosc1010-lab08.py

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Cole Jordan
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
-# Sources, people worked with, help given to:
+# 11/7/2024
+# Lab 08
+# Lab Section: 12
+# Sources, people worked with, help given to: Peter Martinez, Chauncy Hendon
 # your
 # comments
 # here
@@ -14,6 +14,43 @@
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
 
+def decimalChecker(input):
+    numString = input
+    if '.' in numString:
+        numString = numString.replace('.', '', 1)
+        if '.' in numString:
+            return False
+        else:
+            return numString
+    else:
+        return False
+
+def negChecker(input):
+    numString = input
+    if numString.startswith('-'):
+        numString = numString.replace('-', '', 1)
+        if '-' in numString:
+            return False
+        else:
+            return numString
+    else:
+        return False
+    
+def numChecker(input):
+    numString = input
+    isFloat = False
+    if negChecker(input) != False:
+        numString = negChecker(numString)
+    if decimalChecker(input) != False:
+        numString = decimalChecker(numString)
+        isFloat = True
+
+    if numString.isdigit() and isFloat:
+        return float(input)
+    elif numString.isdigit():
+        return int(input)
+    else:
+        return False
 
 print("*" * 75)
 
@@ -33,10 +70,42 @@ print("*" * 75)
 # m, b can be floats or integers
 # the bounds must be integers, if not return false
 
+def pointSlope(m,b,lowerX:int,upperX:int):
+    yList = []
+    if numChecker(m) != False or numChecker(b) != False:
+        m = numChecker(m)
+        b = numChecker(b)
+    elif lowerX > upperX:
+        return False
+    else:
+        return False
+    
+    for i in range(lowerX,upperX):
+        yList.append((m*i) + b)
+
+    return yList
+
+
 # Create a while loop to prompt users for their input for the four variables
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+
+while True:
+    entry = input("Enter 'exit' to exit. Enter the function values as m,b,lower bound,upper bound ")
+    if entry.lower() == 'exit':
+        break
+    
+    try:
+        listInput = entry.split(',')
+        if pointSlope(listInput[0],listInput[1],int(listInput[2]),int(listInput[3]) + 1) == False:
+            print('Invalid Input A')
+            continue
+        else:
+            print(f"Listed Values: {pointSlope(listInput[0],listInput[1],int(listInput[2]),int(listInput[3]) + 1)}")
+    except:
+        print('Invalid Input B')
+        continue
 
 print("*" * 75)
 
@@ -48,3 +117,38 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+
+def squareRoot(input):
+    if input < 0:
+        return 
+    elif input == 0:
+        return 0
+    else:
+        return input**(1/2)
+    
+
+
+def quadraticFormula(a,b,c):
+    result1 = 0
+    result2 = 0
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        result1 = (-b + squareRoot((b**2 - 4*a*c))) / (2*a)
+        result2 = (-b - squareRoot((b**2 - 4*a*c))) / (2*a)
+        return [result1,result2]
+    except:
+        return False
+
+while True:
+    entry = input("Type 'exit' to exit. Enter the quadratic values in the format of a,b,c ")
+    if entry.lower == 'exit':
+        break
+    entry = entry.split(',')
+    if quadraticFormula(entry[0], entry[1], entry[2]) == False:
+        print('Invalid Input')
+        continue
+    
+    print(f"Result: {quadraticFormula(entry[0], entry[1], entry[2])}")
+    
